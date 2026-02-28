cdf744534863f8461889eab3f2d111cea4738784
MANISH YADAV <128881497+Mydvthukran@users.noreply.github.com>
2026-02-28 22:47:21 +0530

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,17 +1,15 @@
+import fetch from "node-fetch";
+
 export default async function handler(req, res) {
   const { username } = req.query;
+  if (!username) return res.status(400).send("Username required");
 
   const response = await fetch(
-    `https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&theme=radical`,
-    {
-      headers: {
-        Authorization: `token ${process.env.PAT_1}`,
-      },
-    }
+    `https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&theme=radical`
   );
 
   const svg = await response.text();
-
   res.setHeader("Content-Type", "image/svg+xml");
+  res.setHeader("Cache-Control", "public, max-age=3600");
   res.status(200).send(svg);
 }
