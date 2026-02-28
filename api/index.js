970d54de9c06f6b1cdecad0292e1bc69c3a143c9
MANISH YADAV <128881497+Mydvthukran@users.noreply.github.com>
2026-02-28 21:24:06 +0530

    Update SVG response with user statistics
    Refactor SVG generation to include user data directly in the response.

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
-import { renderStats } from '../src/renderStats.js'
+res.setHeader("Content-Type", "image/svg+xml");
 
-export default async function handler(req, res) {
-  try {
-    const username = req.query.username
+const svg = `
+<svg width="400" height="200" xmlns="http://www.w3.org/2000/svg">
+  <foreignObject width="100%" height="100%">
+    <div xmlns="http://www.w3.org/1999/xhtml"
+      style="font-family:Arial;padding:10px;
+      background:#0d1117;color:white;border-radius:10px;">
+      
+      <h3>${data.login}</h3>
+      <p>Repos: ${data.public_repos}</p>
+      <p>Followers: ${data.followers}</p>
+      <p>Following: ${data.following}</p>
+      <p>Stars: ${stars}</p>
+      <p>Forks: ${forks}</p>
+      
+    </div>
+  </foreignObject>
+</svg>
+`;
 
-    if (!username) {
-      return res.status(400).send('Username is required')
-    }
-
-    const svg = await renderStats(username, process.env.PAT_1)
-
-    res.setHeader('Content-Type', 'image/svg+xml')
-    res.status(200).send(svg)
-  } catch (err) {
-    res.status(500).send('Something went wrong')
-  }
-}
+res.status(200).send(svg);
