8a46a48fa566aac6f2f2c14c3045c30ecfad49eb
MANISH YADAV <128881497+Mydvthukran@users.noreply.github.com>
2026-02-28 22:15:27 +0530

    Fix authorization token syntax in API request

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
     const result = await fetch("https://api.github.com/graphql", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}$`,
+       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
